1363fb9640977f9f892f2ec88a0b69f165a39ee1
Gaby <gaby@Gabys-MacBook-Pro.local>
2022-12-25 18:00:44 +0100

    ADD TEST

--- 2_discrete_optmization/knapsack.py
+++ 2_discrete_optmization/knapsack.py
@@ -1,7 +1,7 @@
 # x elements with diff weight and value, max value subject to a certain max weight
+# each element can be used only once
 
-# I elementos
-# C capacity
+
 import copy
 
 class Elemento:
@@ -15,15 +15,15 @@ class Elemento:
 
 
 
-class Opciones:
-    def __init__(self, all_elementos, capacidad):
+class Optimal:
+    def __init__(self, all_elementos, capacity):
         self.all_elementos = all_elementos
-        self.capacidad = capacidad
-        self.tabla = [[Bag(c, []) for e in range(len(all_elementos)+1)] for c in range(capacidad+1)]
+        self.capacity = capacity
+        self.tabla = [[Bag(c, []) for _ in range(len(all_elementos)+1)] for c in range(capacity + 1)]
 
 
     def __repre__(self):
-        return str(f"hasta {self.hasta_elemento} elementos, hasta {self.capacidad} capacidad")
+        return str(f"hasta {self.hasta_elemento} elementos, hasta {self.capacity} capacidad")
 
     #def __getitem__(self, item):
 
@@ -35,35 +35,40 @@ class Opciones:
         bag_misma_capacidad_sin_elem = self.tabla[hasta_capacidad][hasta_elemento - 1]
         peso_misma_capacidad_sin_elem = bag_misma_capacidad_sin_elem.suma_pesos()
 
-        valor_1 = 0
-        valor_2 = 0
+        valor_add = 0
+        valor_replace = 0
 
         # mantiene el bag de la misma capacidad sin el elemento
-        bag_0 = copy.copy(bag_misma_capacidad_sin_elem)
-        valor_0 = bag_0.suma_valor()
+        bag_keep = copy.copy(bag_misma_capacidad_sin_elem)
+        valor_keep = bag_keep.suma_valor()
 
         # sumar a la bag anterior (misma capacidad un elemento menos)
         if peso_misma_capacidad_sin_elem + element.peso <= hasta_capacidad:
             elementos_elegidos = copy.copy(bag_misma_capacidad_sin_elem.elementos_elegidos)
             elementos_elegidos.append(element)
-            bag_1 = Bag(capacidad=hasta_capacidad, elementos_elegidos=elementos_elegidos)
-            valor_1 = bag_1.suma_valor()
+            bag_add = Bag(capacidad=hasta_capacidad, elementos_elegidos=elementos_elegidos)
+            valor_add = bag_add.suma_valor()
         # buscar la bag sin este elemento a la que le entra y sumarlo
         if hasta_capacidad - element.peso >= 0:
             bag_sin_capac_para_elem = self.tabla[hasta_capacidad - element.peso][hasta_elemento - 1]
             elementos_elegidos = copy.copy(bag_sin_capac_para_elem.elementos_elegidos)
             elementos_elegidos.append(element)
-            bag_2 = Bag(capacidad=hasta_capacidad, elementos_elegidos=elementos_elegidos)
-            valor_2 = bag_2.suma_valor()
+            bag_replace = Bag(capacidad=hasta_capacidad, elementos_elegidos=elementos_elegidos)
+            valor_replace = bag_replace.suma_valor()
 
-        if valor_1 >= valor_2 and valor_1 > 0 and valor_1 >= valor_0:
-            self.tabla[hasta_capacidad][hasta_elemento] = bag_1
-        elif valor_2 > 0 and valor_2 >= valor_0:
-            self.tabla[hasta_capacidad][hasta_elemento] = bag_2
-        elif valor_0 > 0 and valor_0 >= valor_2:
-            self.tabla[hasta_capacidad][hasta_elemento] = bag_0
+        if valor_add > 0 and valor_add >= valor_keep and valor_add >= valor_replace:
+            self.tabla[hasta_capacidad][hasta_elemento] = bag_add
+        elif valor_replace > 0 and valor_replace >= valor_keep:
+            self.tabla[hasta_capacidad][hasta_elemento] = bag_replace
+        elif valor_keep > 0 and valor_keep >= valor_replace:
+            self.tabla[hasta_capacidad][hasta_elemento] = bag_keep
 
-
+    def get_optimal_bag(self):
+        for hasta_elemento in range(1, len(self.all_elementos) + 1):
+            for hasta_capacidad in range(0, self.capacity + 1):
+                possible_elements = self.get_possible_elements(hasta_elemento)
+                self.evaluate_adding_element(hasta_capacidad, hasta_elemento, possible_elements[-1])
+        return self.tabla
 
 
 
@@ -99,13 +104,29 @@ capacity = 9
 
 # solve a problem for items 1, items 1 and 2 and item1,2,3 iterating over i  in range(0, capacity+1).
 
-elegido = {}
-o = Opciones(all_elementos, capacity)
 
-for hasta_elemento in range(1, len(all_elementos) + 1):
-    for hasta_capacidad in range(0, capacity + 1):
-        possible_elements = o.get_possible_elements(hasta_elemento)
-        o.evaluate_adding_element(hasta_capacidad, hasta_elemento, possible_elements[-1])
+
+def test_kanpsack():
+    all_elementos = [
+        Elemento(1, 4, 5),
+        Elemento(2, 5, 6),
+        Elemento(3, 2, 3)
+    ]
+
+    capacity = 9
+
+    o = Optimal(all_elementos, capacity)
+    tabla = o.get_optimal_bag()
+
+    assert tabla[5][3].suma_valor() == 6
+    assert tabla[5][3].suma_pesos() == 5
+
+    assert tabla[6][3].suma_valor() == 8
+    assert tabla[6][3].suma_pesos() == 6
+
+    assert tabla[9][3].suma_valor() == 11
+    assert tabla[9][3].suma_pesos() == 9
+
 
 
 
