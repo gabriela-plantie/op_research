b728e6f7d79c6e90aaceac021743a921eb4a174e
gaby <5839522+gabriela-plantie@users.noreply.github.com>
2026-06-03 20:45:07 +0200

    format+env

---
+++ 2_discrete_optmization/2_branch_&_bound.py
@@ -0,0 +1,12 @@
+# branch = choose an item or ot
+# bound = finding a bound (optimistic evaluation)
+
+# limited discrepancy search (when you have a good heuristic) -> search!
+
+# DP: dynamic programming space
+
+# branch and bound: the problem is bounding, calculate the bounding
+
+# CP Constraint programming
+
+# LS local search
